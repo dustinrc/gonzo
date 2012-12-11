606a52eef3d949275c5ca16df7bbfbabe2f3aad7
Dustin Chapman <dustin.r.chapman@gmail.com>
2012-12-11 16:10:24 -0500

    formatting

--- mdp/protocol.go
+++ mdp/protocol.go
@@ -4,9 +4,9 @@ const (
 	CV01 = "MDPC01"
 	WV01 = "MDPW01"
 
-	READY = 0x01
-	REQUEST = 0x02
-	REPLY = 0x03
-	HEARTBEAT = 0x04
+	READY      = 0x01
+	REQUEST    = 0x02
+	REPLY      = 0x03
+	HEARTBEAT  = 0x04
 	DISCONNECT = 0x05
 )
