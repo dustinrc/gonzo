d48401541a80937534a723e162d05ee2eebdeddf
Dustin Chapman <dustin.r.chapman@gmail.com>
2012-12-18 16:07:48 -0500

    protocol error checking on reply to client

--- mdp/client/client.go
+++ mdp/client/client.go
@@ -1,6 +1,9 @@
 package client
 
-import "github.com/dustinrc/gonzo/mdp"
+import (
+	"fmt"
+	"github.com/dustinrc/gonzo/mdp"
+)
 
 type Client interface {
 	Send(service string, message mdp.Message) (mdp.Message, error)
@@ -9,6 +12,16 @@ type Client interface {
 
 type client struct {
 	conn *connection
+}
+
+type clientMismatchError struct {
+	mismatchType string
+	want, received interface{}
+}
+
+func (e clientMismatchError) Error() string {
+	return fmt.Sprintf("%v mismatch: want \"%v\", received \"%v\"", e.mismatchType,
+		e.want, e.received)
 }
 
 func New(brokerURL string, timeout float64) (Client, error) {
@@ -38,6 +51,13 @@ func (c *client) Send(service string, message mdp.Message) (mdp.Message, error) 
 		return nil, err
 	}
 
+	if proto := string(reply[0]); proto != mdp.CV01 {
+		err = clientMismatchError{"protocol", mdp.CV01, proto}
+		return nil, err
+	} else if srvc := string(reply[1]); srvc != service {
+		err = clientMismatchError{"service", service, srvc}
+		return nil, err
+	}
 	reply = reply[2:]
 	return reply, nil
 }
