3517779638acf7ff7e3602c879e72624b5bb0353
Dustin Chapman <dustin.r.chapman@gmail.com>
2013-01-07 09:56:14 -0500

    use iota for constants

--- mdp/protocol.go
+++ mdp/protocol.go
@@ -4,9 +4,10 @@ const (
 	CV01 = "MDPC01"
 	WV01 = "MDPW01"
 
-	READY      = 0x01
-	REQUEST    = 0x02
-	REPLY      = 0x03
-	HEARTBEAT  = 0x04
-	DISCONNECT = 0x05
+	_ byte = iota
+	READY
+	REQUEST
+	REPLY
+	HEARTBEAT
+	DISCONNECT
 )
